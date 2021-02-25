2ddc2977ee75d26ec68ae31cf52df8bf2ebc9ffb
DaggerCR <dagger@estudiantec.cr>
2021-02-25 13:13:06 -0600

    .cpp inicial
    Con los primeros structs

---
+++ AsciiContador.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+
+struct Nodo
+{
+	char ascii[1];
+	int numAscii;
+	int cantidad;
+	Nodo siguiente;
+	
+};
+
+struct listaSimple
+{
+	Nodo primerNodo;
+	Nodo ultimoNodo;
+};
+
+void main()
+{
+	
+	
+	
+}
+
