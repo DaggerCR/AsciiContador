467ac6d0ccf3b07e55455bdd31cf24a95131688a
DaggerCR <dagger@estudiantec.cr>
2021-02-25 19:10:06 -0600

    Se creó el txt y más...
    se creo el txt a ser leído
    en el cpp se terminaron los structs (beta)

--- AsciiContador.cpp
+++ AsciiContador.cpp
@@ -2,25 +2,52 @@
 #include<conio.h>
 #include<iostream>
 
+using namespace std;
+
 struct Nodo
 {
 	char ascii[1];
 	int numAscii;
 	int cantidad;
-	Nodo siguiente;
+	Nodo *siguiente = NULL;
+	Nodo *anterior = NULL;
 	
 };
 
 struct listaSimple
 {
-	Nodo primerNodo;
-	Nodo ultimoNodo;
+	Nodo *primerNodo;
+	Nodo *ultimoNodo;
+	
+	listaSimple()
+	{
+		//constructor del struct
+		primerNodo = NULL;
+		ultimoNodo = NULL;
+	}
+	
+	add(Nodo *qNodo)
+	{
+		if (primerNodo == NULL)
+     	{
+        	primerNodo = qNodo;
+         	ultimoNodo = primerNodo; // ambos apuntan al nuevo               
+     	}
+     	else
+     	{	
+         	ultimoNodo->siguiente = qNodo;
+         	ultimoNodo = qNodo;	
+     	}
+	}
+	
+	
 };
 
-void main()
+int main()
 {
+	listaSimple *lista = new listaSimple();
 	
-	
-	
-}
+
+	cout << "programa finalizado con éxito";	
+return 0;}
 
